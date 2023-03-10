7bbb3eda963707c9074ddb30e6c8ea3d2f8efb45
zihanqiu <zihanqiu@umich.edu>
2023-03-10 16:27:24 -0500

    Change the logics in the while loop

--- speech1.py
+++ speech1.py
@@ -34,53 +34,46 @@ def light_off(pin):
 
 while(True):
     board.digital[trigger_pin] = HIGH
-    time.delayMicroseconds(10)
+    time.sleep(0.00001)
     board.digital[trigger_pin] = LOW
 
-    t = time.pulseIn(echo_pin, HIGH)
+    pulse_duration = board.digital[echo_pin].pulse_read()
 
-    distance = (t * 0.034) / 2
+    distance = pulse_duration * 34300 / 2
 
     if distance <= 10:
-        board.digital[led_pin] = HIGH
-        time.delay(500)
-    else:
-        board.digital[led_pin] = LOW
-        time.delay(500)
-    # obtain audio from the microphone
-    engine = pyttsx3.init()
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Say something!")
-        engine.say("Say something")
-        engine.runAndWait()
-        r.adjust_for_ambient_noise(source, duration=1)
-        audio = r.listen(source, phrase_time_limit=3)
-
-    # recognize speech using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        text = r.recognize_google(audio, language='en-in')
-        print("Google Speech Recognition thinks you said " + text)
-        if (text == "open the door"):
-            print("door is open!!")
-            engine.say("door is open")
+        # obtain audio from the microphone
+        engine = pyttsx3.init()
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Say something!")
+            engine.say("Say something")
             engine.runAndWait()
-            light_on(pin)
-        elif (text == "close the door"):
-            print("door is closed!!")
-            engine.say("door is closed")
-            engine.runAndWait()
-            light_off(pin)
-    except sr.UnknownValueError:
-        engine.say("Google Speech Recognition could not understand audio")
-        print("I cannot undersatand")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            r.adjust_for_ambient_noise(source, duration=1)
+            audio = r.listen(source, phrase_time_limit=3)
 
-
+        # recognize speech using Google Speech Recognition
+        try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+            text = r.recognize_google(audio, language='en-in')
+            print("Google Speech Recognition thinks you said " + text)
+            if (text == "open the door"):
+                print("door is open!!")
+                engine.say("door is open")
+                engine.runAndWait()
+                light_on(pin)
+            elif (text == "close the door"):
+                print("door is closed!!")
+                engine.say("door is closed")
+                engine.runAndWait()
+                light_off(pin)
+        except sr.UnknownValueError:
+            engine.say("Google Speech Recognition could not understand audio")
+            print("I cannot undersatand")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 # noicse cancellation
 # trigger proximity-ultra sonic / run the program
